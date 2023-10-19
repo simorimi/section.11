9c028ba110ab2a302a68f3cad2f4fc6fc0e8a986
simorimi <hori5020@gmail.com>
2023-10-19 21:52:23 +0900

    section.11 final version

---
+++ app.js
@@ -0,0 +1,29 @@
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = { title: "Developer", place: "New York", salary: 50000 };
+
+function calculateAdultYears(userAge) {
+    let result;
+    result = userAge - 18;
+    return result;
+}
+
+let totalAdultYears = calculateAdultYears(age);
+
+console.log(totalAdultYears);
+
+age = 45;
+
+totalAdultYears = calculateAdultYears(age);
+
+console.log(totalAdultYears);
+
+let person = {
+    name: 'Max',
+    greet() {
+        console.log('Hello');
+    }
+};
+
+person.greet();
---
+++ operations.js
@@ -0,0 +1,30 @@
+console.log(10+4);
+console.log(10-4);
+console.log(10*4);
+console.log(10/4);
+console.log(10%4); //modulus operation
+console.log(10%3);
+
+console.log((10 + 3 - 5) * 10);
+
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result++; // result = result + 1
+result--; // result = result - 1
+
+result+=5; // result = result + 5
+result-=5; // result = result - 5
+result/=5; // result = result / 5
+result*=5; // result = result * 5
+console.log(result);
+
+console.log('Max' + ' ' + 'Schwarzmuller');
+console.log('Max' - 'ax');
+console.log('Max'*3); //python과 달리 문자열 곱하기도 안됨
+let userName = 'Max';
+
+console.log(userName.length);
+console.log(userName.toLocaleUpperCase());
+
+let hobbies = ['Sports', 'Cooking'];
+console.log(hobbies.length);
